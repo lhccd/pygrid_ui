dc7a03e907935d3c26f6fbbee1a6104ce190d315
Lorenz <67464618+lhccd@users.noreply.github.com>
2021-12-07 11:46:41 +0100

    cleared users page

--- components/Grid.js
+++ components/Grid.js
@@ -1,7 +1,7 @@
 import tw from 'twin.macro'
 
 const Grid = props => (
-    <div {...props} tw='grid grid-cols-12 gap-2 px-10 min-h-full w-full'/>
+    <div {...props} tw='grid grid-cols-12 gap-2 px-10'/>
 )
 
 export { Grid }
--- pages/users.js
+++ pages/users.js
@@ -39,97 +39,8 @@ function DomainInfo(){
     )
 }
 
-
-// function Users1({ ninjas }){
-
-//     console.log(ninjas)
-
-//     return (
-//         <div tw="relative min-h-screen flex">
-//             <div tw="flex flex-col justify-between bg-gradient-to-r from-black to-gray-800 text-gray-200 w-64 py-6">
-//                 <DomainInfo tw="p-6"/>
-//                 <div tw="my-10"></div>
-//                 <nav tw="text-lg mb-auto">
-//                     <ul tw="space-y-8">
-//                         <Link href="/dashboard">
-//                             <div tw="flex items-center space-x-3 px-6 hover:bg-gray-500">   
-//                                 <div tw="p-2">
-//                                     <FontAwesomeIcon size="sm" icon={faThLarge}/>
-//                                 </div>
-//                                 <a>Dashboard</a>
-//                             </div>
-//                         </Link>
-//                         <Link href="/users">
-//                             <div tw="flex items-center space-x-3 px-6 hover:bg-gray-500"> 
-//                                 <div tw="p-2">  
-//                                     <FontAwesomeIcon size="sm" icon={faUsers}/>
-//                                 </div>
-//                                 <a>Users</a>
-//                                 </div>
-//                         </Link>
-//                         <Link href="/permissions">
-//                             <div tw="flex items-center space-x-3 px-6 hover:bg-gray-500">  
-//                                 <div tw="p-2"> 
-//                                     <FontAwesomeIcon size="sm" icon={faCheck}/>
-//                                 </div>        
-//                                 <a>Permissions</a>
-//                             </div>
-//                         </Link>
-//                         <Link href="/requests">
-//                             <div tw="flex items-center space-x-3 px-6 hover:bg-gray-500">   
-//                                 <div tw="p-2">
-//                                     <FontAwesomeIcon size="sm" icon={faLemon}/>
-//                                 </div>
-//                                 <a>Requests</a>
-//                                 </div>
-//                         </Link>
-//                         <Link href="/networks">
-//                             <div tw="flex items-center space-x-3 px-6 hover:bg-gray-500">   
-//                                 <div tw="p-2">
-//                                     <FontAwesomeIcon size="sm" icon={faHandsHelping}/>
-//                                 </div>
-//                                 <a>Networks</a>
-//                             </div>
-//                         </Link>
-//                     </ul>
-//                 </nav>
-//                 <footer tw="text-lg space-y-10">
-//                     <DomainConnectionStatus/>
-//                     <div tw="flex items-center space-x-3 px-6 hover:bg-gray-500">
-//                         <div>
-//                             <FontAwesomeIcon icon={faUserCircle} size="sm"/>
-//                         </div>
-//                         <p tw="">Kyoko Eng</p>
-//                     </div>
-//                 </footer>
-//             </div>
-//             <div tw="flex-1 p-10 text-2xl font-bold">
-//                 <div id="grid" tw="grid grid-cols-12 grid-rows-6 gap-2 px-10 m-5">
-//                     {ninjas.map(ninja => (
-//                         <div key={ninja.id} tw="bg-primary-200 col-span-3 text-white p-3 rounded-2xl">
-//                             <div key={ninja.id} >{ninja.name}</div>
-//                             <div key={ninja.phone} >{ninja.address.city}</div>
-//                         </div>
-//                     ))}
-//                     {ninjas.map(ninja => (
-//                         <div key={ninja.id} tw="bg-primary-200 row-span-3 col-span-2 text-white p-3 rounded-2xl">
-//                             <div key={ninja.id} >{ninja.name}</div>
-//                             <div key={ninja.phone} >{ninja.address.city}</div>
-//                         </div>
-//                     ))}
-//                     {ninjas.map(ninja => (
-//                         <div key={ninja.id} tw="bg-primary-200 col-span-6 text-white p-3 rounded-2xl">
-//                             <div key={ninja.id} >{ninja.name}</div>
-//                             <div key={ninja.phone} >{ninja.address.city}</div>
-//                         </div>
-//                     ))}
-//                 </div>
-//             </div>
-//         </div>
-//     )
-// }
-
 export default function Users({ninjas}){
+    console.log({ninjas})
     return (
         <Layout>
             {ninjas.map(ninja => (
@@ -140,6 +51,42 @@ export default function Users({ninjas}){
             ))}
             {ninjas.map(ninja => (
                 <div key={ninja.id} tw="bg-primary-200 row-span-3 col-span-2 text-white p-3 rounded-2xl">
+                    <div key={ninja.id} >{ninja.name}</div>
+                    <div key={ninja.phone} >{ninja.address.city}</div>
+                </div>
+            ))}
+            {ninjas.map(ninja => (
+                <div key={ninja.id} tw="bg-primary-200 col-span-6 text-white p-3 rounded-2xl">
+                    <div key={ninja.id} >{ninja.name}</div>
+                    <div key={ninja.phone} >{ninja.address.city}</div>
+                </div>
+            ))}
+            {ninjas.map(ninja => (
+                <div key={ninja.id} tw="bg-primary-200 row-span-3 col-span-2 text-white p-3 rounded-2xl">
+                    <div key={ninja.id} >{ninja.name}</div>
+                    <div key={ninja.phone} >{ninja.address.city}</div>
+                </div>
+            ))}
+            {ninjas.map(ninja => (
+                <div key={ninja.id} tw="bg-primary-200 col-span-12 text-white p-3 rounded-2xl">
+                    <div key={ninja.id} >{ninja.name}</div>
+                    <div key={ninja.phone} >{ninja.address.city}</div>
+                </div>
+            ))}
+            {ninjas.map(ninja => (
+                <div key={ninja.id} tw="bg-primary-200 row-span-5 col-span-2 text-white p-3 rounded-2xl">
+                    <div key={ninja.id} >{ninja.name}</div>
+                    <div key={ninja.phone} >{ninja.address.city}</div>
+                </div>
+            ))}
+            {ninjas.map(ninja => (
+                <div key={ninja.id} tw="bg-primary-200 col-span-4 text-white p-3 rounded-2xl">
+                    <div key={ninja.id} >{ninja.name}</div>
+                    <div key={ninja.phone} >{ninja.address.city}</div>
+                </div>
+            ))}
+            {ninjas.map(ninja => (
+                <div key={ninja.id} tw="bg-primary-200 row-span-4 col-span-4 text-white p-3 rounded-2xl">
                     <div key={ninja.id} >{ninja.name}</div>
                     <div key={ninja.phone} >{ninja.address.city}</div>
                 </div>
